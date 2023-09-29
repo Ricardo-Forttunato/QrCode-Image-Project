675a0e71261b3b9a29a3373ca78637d7235a2661
João Ricardo <ricardo.forttunato@gmail.com>
2023-09-29 16:24:35 -0300

    console.log error fix

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 
 import inquirer from 'inquirer';
 import qr from 'qr-image';
@@ -11,7 +6,7 @@ import fs from 'fs';
 inquirer
   .prompt([
     {
-        message: "Type in a URL to make a QR code for:",
+        message: "Type in a URL to make a QR code:",
         name: "url"
     }
   ])
@@ -27,9 +22,9 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
+      console.log("Prompt couldn't be rendered in the current environment");
     } else {
-      // Something else went wrong
+      console.log(error);
     }
   });
 
